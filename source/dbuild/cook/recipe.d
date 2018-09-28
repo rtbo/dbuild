5aa8838d4089fe598426f697826f998ea5059af8
Remi Thebault <remi.thebault@gmail.com>
2018-09-29 00:17:29 +0200

    recipe write/load file

--- source/dbuild/cook/recipe.d
+++ source/dbuild/cook/recipe.d
@@ -209,3 +209,187 @@ struct Recipe
         return this;
     }
 }
+
+void writeToFile(Recipe recipe, string filename)
+{
+    import std.exception : enforce;
+    import std.stdio : File;
+
+    auto f = File(filename, "w");
+    foreach (r; recipe.rules) {
+        enforce(r.commandDg is null, "cannot write rules with delegate");
+        f.writeln("rule ", r.name);
+        f.writeln("\tdescription ", r.description);
+        f.writeln("\tcommand ", r.command);
+        if (r.deps != Deps.none)
+            f.writeln("\tdeps ", r.deps);
+        if (r.depfile.length)
+            f.writeln("\tdepfile ", r.depfile);
+        if (r.jobs != 1)
+            f.writeln("\tjobs ", r.jobs);
+        f.writeln();
+    }
+
+    void writeArray(in string key, in string[] values) {
+        foreach (v; values) {
+            f.writefln("\t%s %s", key, v);
+        }
+    }
+
+    foreach (b; recipe.builds) {
+        f.writeln("build ", b.rule);
+        writeArray("input", b.inputs);
+        writeArray("implicitInput", b.implicitInputs);
+        writeArray("orderOnlyInput", b.orderOnlyInputs);
+        writeArray("output", b.outputs);
+        writeArray("implicitOutput", b.implicitOutputs);
+        if (b.jobs != 0) f.writeln("\tjobs ", b.jobs);
+        foreach (k, v; b.bindings) {
+            if (k.length && v.length)
+                f.writefln("\tbinding %s = %s", k, v);
+        }
+        f.writeln();
+    }
+
+    foreach (k, v; recipe.bindings) {
+        if (k.length && v.length) {
+            f.writefln("binding %s = %s", k, v);
+        }
+    }
+
+    // take the direct field here, otherwise we get getcwd
+    if (recipe._cacheDir.length) {
+        f.writefln("cacheDir %s", recipe._cacheDir);
+    }
+}
+
+class RecipeReadException : Exception
+{
+    string filename;
+    int lineNum;
+
+    this (string filename, int lineNum, string msg)
+    {
+        this.filename = filename;
+        this.lineNum = lineNum;
+        super(msg);
+    }
+}
+
+Recipe loadFromFile(string filename)
+{
+    import std.algorithm : startsWith;
+    import std.conv : to;
+    import std.exception : enforce;
+    import std.stdio : File;
+    import std.string : stripLeft;
+    import std.uni : isWhite;
+
+    auto f = File(filename, "r");
+
+    Recipe recipe;
+    Rule rule = void;
+    Build build = void;
+    bool inRule, inBuild;
+    int lineNum;
+
+    foreach (l; f.byLineCopy) {
+        ++lineNum;
+        if (!inRule && !inBuild) {
+            if (!l.length) continue;
+            if (l.startsWith("rule ")) {
+                inRule = true;
+                rule = Rule.init;
+                rule._name = l["rule ".length .. $];
+            }
+            else if (l.startsWith("build ")) {
+                inBuild = true;
+                build = Build.init;
+                build._rule = l["build ".length .. $];
+            }
+            else if (l.startsWith("binding ")) {
+                auto b = readBinding(l["binding ".length .. $], filename, lineNum);
+                recipe.bindings[b[0]] = b[1];
+            }
+            else if (l.startsWith("cacheDir ")) {
+                recipe._cacheDir = l["cacheDir ".length .. $];
+            }
+            else {
+                throw new RecipeReadException(filename, lineNum, "unexpected input: "~l);
+            }
+        }
+        else if (inRule) {
+            if (!l.length) {
+                recipe.rules ~= rule;
+                inRule = false;
+                continue;
+            }
+            enforce(l[0].isWhite);
+            l = l.stripLeft();
+            if (l.startsWith("description ")) {
+                rule._description = l["description ".length .. $];
+            }
+            else if (l.startsWith("command ")) {
+                rule._command = l["command ".length .. $];
+            }
+            else if (l.startsWith("depfile ")) {
+                rule._depfile = l["depfile ".length .. $];
+            }
+            else if (l.startsWith("deps ")) {
+                rule._deps = l["deps ".length .. $].to!Deps;
+            }
+            else if (l.startsWith("jobs ")) {
+                rule._jobs = l["jobs ".length .. $].to!uint;
+            }
+            else {
+                throw new RecipeReadException(filename, lineNum, "unexpected input: "~l);
+            }
+        }
+        else if (inBuild) {
+            if (!l.length) {
+                recipe.builds ~= build;
+                inBuild = false;
+                continue;
+            }
+            enforce(l[0].isWhite);
+            l = l.stripLeft();
+            if (l.startsWith("input ")) {
+                build._inputs ~= l["input ".length .. $];
+            }
+            else if (l.startsWith("implicitInput ")) {
+                build._implicitInputs ~= l["implicitInput ".length .. $];
+            }
+            else if (l.startsWith("orderOnlyInput ")) {
+                build._orderOnlyInputs ~= l["orderOnlyInput ".length .. $];
+            }
+            else if (l.startsWith("output ")) {
+                build._outputs ~= l["output ".length .. $];
+            }
+            else if (l.startsWith("implicitOutput ")) {
+                build._implicitOutputs ~= l["implicitOutput ".length .. $];
+            }
+            else if (l.startsWith("binding ")) {
+                auto b = readBinding(l["binding ".length .. $], filename, lineNum);
+                build._bindings[b[0]] = b[1];
+            }
+            else if (l.startsWith("jobs ")) {
+                build._jobs = l["jobs ".length .. $].to!uint;
+            }
+        }
+    }
+
+    return recipe;
+}
+
+private string[2] readBinding(string str, string filename, int lineNum)
+{
+    import std.algorithm : findSplit;
+    import std.exception : enforce;
+    import std.string : strip;
+
+    auto split = findSplit(str, " = ");
+    enforce(split[0].length && split[2].length,
+        new RecipeReadException(filename, lineNum, "Error reading binding: "~str)
+    );
+    return [ split[0].strip(), split[2].strip() ];
+}
