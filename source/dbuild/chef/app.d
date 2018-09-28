45d0f2bb986c884afa81cbcd211035013aa7e733
Remi Thebault <remi.thebault@gmail.com>
2018-09-28 08:19:52 +0200

    adding chef and cook apps

---
+++ source/dbuild/chef/app.d
@@ -0,0 +1,8 @@
+module dbuild.chef.app;
+
+version(ChefApp):
+
+int main (in string[] args)
+{
+    return 0;
+}
---
+++ source/dbuild/cook/app.d
@@ -0,0 +1,8 @@
+module dbuild.cook.app;
+
+version(CookApp):
+
+int main (in string[] args)
+{
+    return 0;
+}
